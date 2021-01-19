2ad7082ee39ee6a8ca7f8690bf3f72c5fcaa9548
Yifan Wang <wangyf@udel.edu>
2021-01-18 19:11:23 -0500

    uppate experiment section

--- nextorch/experiment.py
+++ nextorch/experiment.py
@@ -85,7 +85,101 @@ def fit_with_new_observations(model: Model, X: Tensor, Y: Tensor) -> Model:
     model.eval()
     
     return model
+
+#%% Model training helper functions
+def evaluate_objective_func(
+    X_unit: MatrixLike2d, 
+    X_range: ArrayLike1d, 
+    objective_func: object
+) -> Tensor:
+    """Evaluate the test function
+
+    Parameters
+    ----------
+    X_unit : MatrixLike2d, matrix or 2d tensor
+        X in a unit scale
+    X_range : ArrayLike1d, array or 1d tensor
+        list of x ranges
+    test_function : function object
+        a test function which evaluate np arrays
+
+    Returns
+    -------
+    y_tensor: Tensor
+        model predicted values
+    """
+    # Convert matrix type from tensor to numpy matrix
+    if isinstance(X_unit, torch.Tensor):
+        X_unit_np = X_unit.cpu().numpy()
+    else:
+        X_unit_np = X_unit.copy()
+        
+    if isinstance(X_range, torch.Tensor):
+        X_range_np = X_range.cpu().numpy()
+    else:
+        X_range_np = X_range.copy()
+    # transform to real scale 
+    X_real = inverse_unitscale_X(X_unit_np, X_range_np)
+    # evaluate y
+    y = objective_func(X_real)
+    # Convert to tensor
+    y_tensor = torch.tensor(y, dtype = dtype)
+
+    return y_tensor
     
+    
+def _predict_pipeline_for_training(model, train_X, Y_mean, Y_std):
+    
+    '''
+    Predict the output value
+    return the mean, confidence interval in numpy arrays
+    '''
+    train_y, train_y_lower,  train_y_upper = ut.predict_surrogate(model, train_X)
+    
+    train_y_real = ut.inversestandardize_X(train_y, Y_mean, Y_std)
+    train_y_real = train_y_real.detach().numpy()
+    
+    train_y_lower_real = ut.inversestandardize_X(train_y_lower, Y_mean, Y_std)
+    train_y_lower_real = train_y_lower_real.detach().numpy()
+    
+    train_y_upper_real = ut.inversestandardize_X(train_y_upper, Y_mean, Y_std)
+    train_y_upper_real = train_y_upper_real.detach().numpy()
+    
+    return train_y_real, train_y_lower_real, train_y_upper_real
+        
+def predict_surrogate(model, test_x):
+    '''
+    Input a Gaussian process model 
+    return the mean, lower confidence interval and upper confidence intervale 
+    from the postierior
+    '''
+    
+    test_x = copy.deepcopy(test_x)
+    test_x = torch.tensor(test_x, dtype = dtype)
+    
+    posterior = model.posterior(test_x)
+    
+    test_y_mean = posterior.mean
+    test_y_lower, test_y_upper = posterior.mvn.confidence_region()
+    
+    return test_y_mean, test_y_lower, test_y_upper
+
+def _predict_pipeline_for_testing(model, test_X,  mesh_size, Y_mean, Y_std):
+    
+    '''
+    Predict 2d mesh values from surrogates 
+    Generate plots if required
+    Return outputs Y in 2d numpy matrix 
+    '''
+    # predict the mean for ff model, returns a standardized 1d tensor
+    test_y, test_y_lower, test_y_upper = ut.predict_surrogate(model, test_X)
+    # Inverse the standardization and convert 1d y into a 2d array
+    test_Y_real = ut.transform_plot2D_Y(test_y, Y_mean, Y_std, mesh_size)
+    
+    return test_Y_real
+
+    
+
 
 class Experiment():
     """Experiment object, consists of a set of trial points"""
